03f57b90d65727947e19e554c91c33676e308bc6
Anael <anaelj@gmail.com>
2020-08-05 00:20:09 -0400

    alterado save cacheprovider estava causando erro no deploy

--- src/shared/container/providers/CacheProvider/fakes/FakeCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/fakes/FakeCacheProvider.ts
@@ -8,7 +8,7 @@ export default class FakeCacheProvider implements ICacheProvider {
   private cache: ICacheData = {};
 
   public async save(key: string, value: any): Promise<void> {
-    this.cache[key] - JSON.stringify(value);
+    // this.cache[key] - JSON.stringify(value);
   }
 
   public async recover<T>(key: string): Promise<T | null> {
