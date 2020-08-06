64a5c36a6d4f2ea97add361b0f4fc5b11a1ac646
Anael Medeiros <anaelj@gmail.com>
2020-08-06 13:50:01 -0400

    deploy

--- ormconfig.js
+++ ormconfig.js
@@ -5,13 +5,13 @@ module.exports =
       "type" : "postgres",
         "url" : process.env.DATABASE_URL,
         "entities": [
-            "./dist/modules/**/infra/typeorm/entities/*.js"
+            "dist/modules/**/infra/typeorm/entities/*.js"
         ],
         "migrations" : [
-            "./dist/shared/infra/typeorm/migrations/*.js"
+            "dist/shared/infra/typeorm/migrations/*.js"
         ],
         "cli": {
-          "migrationsDir": "./dist/shared/infra/typeorm/migrations"
+          "migrationsDir": "dist/shared/infra/typeorm/migrations"
         }
     }
     ,
